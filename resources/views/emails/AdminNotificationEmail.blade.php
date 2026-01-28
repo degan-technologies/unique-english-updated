fd8c7c9b4d022c336af4bb4662c0ee4dcb397c08
BiniyamLamesgen <biniyamlamesgen69@gmail.com>
2026-01-28 10:21:10 +0300

    fix: Update logo URL in email templates to reflect new domain

--- resources/views/emails/AdminNotificationEmail.blade.php
+++ resources/views/emails/AdminNotificationEmail.blade.php
@@ -135,7 +135,7 @@
     <div class="email-container">
         <!-- Header Section -->
         <div class="email-header">
-            <img src="https://unique-english.com/images/logo.jpg" alt="Company Logo">
+            <img src="https://unique.degantechnologies.com/images/logo.jpg" alt="Company Logo">
             <h1>New Course Enrollment</h1>
         </div>
 
